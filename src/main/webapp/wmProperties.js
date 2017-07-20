fed03ee5b4dbdb6a6a27efd7091b4d53a68f0638
root <root@61949b3d2f95>
2017-07-20 05:41:50 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_tTrHQCftoR",
+  "homePage" : "Main",
+  "name" : "Auto_tTrHQCftoR",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
